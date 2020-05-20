c37330dd95794a56b027b5d69db44d4ba0579715
Mandy <mandylumjm@gmail.com>
2020-05-20 19:18:04 +0800

    Unit testing for sing() method
    20/05/2020

---
+++ src/code/Assignment/Singtel/Test1.java
@@ -0,0 +1,14 @@
+package code.Assignment.Singtel;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class Test1 {
+	Bird bird = new Bird();
+	@Test
+	public void testSing() { // Q1a
+		bird.sing();
+	}
+
+}
